7ee87a427368f586503079eec56bdd7edadbf118
apxtj <yamato.mizoguchi1@gmail.com>
2024-09-26 13:53:42 +0900

    solved: ListNode勉強中 試作

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+
+        while(l1 != null || l2 != null){
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum/10;
+            current.next = new ListNode(sum%10);
+            current = current.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+
+        if(carry > 0){
+            current.next = new ListNode(carry);
+        }
+        
+        return result.next;
+    }
+}
+// @lc code=end
+
