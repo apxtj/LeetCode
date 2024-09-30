e20c71df5bf6c9db915839b0c301ba92dcbb4d9d
apxtj <yamato.mizoguchi1@gmail.com>
2024-09-30 21:55:29 +0900

    solved: 失敗パターンを入れてみると吉

---
+++ 35.search-insert-position.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int high=nums.length-1;
+        int low=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(nums[mid]>target){
+                high=mid-1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return low;
+    }
+}
+// @lc code=end
+
