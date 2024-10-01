69db68f11b53b8a3de21394516a185548c094704
apxtj <yamato.mizoguchi1@gmail.com>
2024-10-02 01:14:25 +0900

    solved: もう少しスマートに書ける気がする

---
+++ 67.add-binary.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry=0;
+        while(i>=0||j>=0){
+            int sum=0;
+            if(i>=0){
+                sum += Integer.parseInt(String.valueOf(a.charAt(i--)));
+            }
+            if(j>=0){
+                sum += Integer.parseInt(String.valueOf(b.charAt(j--)));
+            }
+            sum+=carry;
+            result.append(sum%2);
+            carry = sum>1 ? 1:0;
+        }
+        if(carry!=0) result.append(carry);
+        return result.reverse().toString();
+    }
+}
+// @lc code=end
+
